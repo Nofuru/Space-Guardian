ce095dfe7f82abb90ccdfec3815e80b900eb190f
Nofuru <71150063+Nofuru@users.noreply.github.com>
2022-07-04 18:13:32 +0300

    Update ColorKeeper.cs

--- Assets/Scripts/ColorKeeper.cs
+++ Assets/Scripts/ColorKeeper.cs
@@ -4,19 +4,9 @@ using UnityEngine;
 public class ColorKeeper : MonoBehaviour
 {
     public static int savedColor = 0;
-    public static List<int> purchasedColors = new List<int>();
 
     private void Awake()
     {
         savedColor = PlayerPrefs.GetInt("SavedColor");
-        SavePurchasedColors();
-    }
-
-    private void SavePurchasedColors()
-    {
-        if (!purchasedColors.Contains(savedColor) && savedColor != 0)
-        {
-            purchasedColors.Add(savedColor);
-        }
     }
 }
